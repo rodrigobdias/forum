e2ec4feaa92fe308ccea557255720fb5f17608cf
Rodrigo <rodrigobdias@gmail.com>
2019-01-23 11:08:02 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Spring Framework 
+Caelum: Curso FJ-27
+
+https://start.spring.io/
+
+
---
+++ src/main/java/br/com/alura/forum/AluraForumApplication.java
@@ -0,0 +1,14 @@
+package br.com.alura.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraForumApplication.class, args);
+	}
+
+}
+
