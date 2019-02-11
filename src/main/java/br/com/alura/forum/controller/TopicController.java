2f363ed3598e0415273269db3a4dc242d9ef60f0
Rodrigo <rodrigobdias@gmail.com>
2019-02-11 11:32:08 -0200

    Acessando dados com Srping Data JPA

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -1,32 +1,26 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.MediaType;
 
 import br.com.alura.forum.controller.dto.output.TopicBriefOutputDto;
-import br.com.alura.forum.model.Category;
-import br.com.alura.forum.model.Course;
-import br.com.alura.forum.model.User;
+import br.com.alura.forum.controller.repository.TopicRepository;
 import br.com.alura.forum.model.topic_domain.Topic;
 
 @RestController
 public class TopicController {
 
+	@Autowired
+	private TopicRepository topicRepository;
+	
 	@GetMapping(value="/api/topics", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<TopicBriefOutputDto> listTopics(){
 		
-		Category subcategory = new Category("Java", new Category("Programação"));
-		Course course = new Course("Java e JSF", subcategory);
-		Topic topic = new Topic("Problemas com o JSF", 
-								null, 
-								new User("Fulano", "fulano@gmail.com", "123456"), 
-								course);
-		
-		List<Topic> topics = Arrays.asList(topic, topic, topic);
+		List<Topic> topics = topicRepository.findAll();
 		
 		return TopicBriefOutputDto.listFromTopics(topics);	
 	}
---
+++ src/main/java/br/com/alura/forum/controller/repository/TopicRepository.java
@@ -0,0 +1,17 @@
+package br.com.alura.forum.controller.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+
+import br.com.alura.forum.model.topic_domain.Topic;
+
+public interface TopicRepository extends Repository<Topic, Long>{
+	
+	@Query("select t from Topic t")
+	List<Topic> list();
+	
+	List<Topic> findAll();
+
+}
