9b680ed47f25213db5cba41b8fc2dcd9ab46c9bb
Rodrigo <rodrigobdias@gmail.com>
2019-02-14 12:21:26 -0200

    Capítulo 7: Documentando e Interagindo com nossos ENDPOINTS

--- src/main/java/br/com/alura/forum/configuration/CorsConfiguration.java
+++ src/main/java/br/com/alura/forum/configuration/CorsConfiguration.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer{
 	
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
+    private static final Logger logger = LoggerFactory.getLogger(CorsConfiguration.class);
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {	    
 		registry.addMapping("/api/**")
 			.allowedOrigins("http://localhost:3000")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-	}
+		
+		logger.info("CORS configurations has been registered");
+    }
 
 }
--- src/main/java/br/com/alura/forum/controller/HomeController.java
+++ src/main/java/br/com/alura/forum/controller/HomeController.java
@@ -1,10 +1,12 @@
 package br.com.alura.forum.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@CrossOrigin
 public class HomeController {
 
 	@RequestMapping("/")
