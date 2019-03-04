24f3ec60819ff1ac1cad82e8db4d9d9e107da278
Rodrigo <rodrigobdias@gmail.com>
2019-03-04 10:52:11 -0300

    Capítulo 11.1: Agendamento tarefas com SPRING

--- src/main/java/br/com/alura/forum/repository/TopicRepository.java
+++ src/main/java/br/com/alura/forum/repository/TopicRepository.java
@@ -3,12 +3,13 @@ package br.com.alura.forum.repository;
 import java.time.Instant;
 import java.util.List;
 
-import br.com.alura.forum.model.User;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.Repository;
 import org.springframework.data.repository.query.Param;
 
+import br.com.alura.forum.model.OpenTopicsByCategory;
+import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic_domain.Topic;
 
 public interface TopicRepository extends Repository<Topic, Long>, JpaSpecificationExecutor<Topic> {
@@ -47,4 +48,13 @@ public interface TopicRepository extends Repository<Topic, Long>, JpaSpecificati
 	int countUnansweredTopicsByCategoryId(@Param("categoryId") Long categoryId);
 
 	List<Topic> findByOwnerAndCreationInstantAfterOrderByCreationInstantAsc(User owner, Instant creationTime);
+	
+	@Query("select new br.com.alura.forum.model.OpenTopicsByCategory(" +
+			"t.course.subcategory.category.name as categoryName, " +
+			"count(t) as topicCount, " +
+			"now() as instant) from Topic t " +
+			"where t.status = 'NOT_ANSWERED' " +
+			"group by t.course.subcategory.category")
+			List<OpenTopicsByCategory> findOpenTopicsByCategory();
+	
 }
