633a577b86c681046e5cb517f8e660c01a99a4c2
Rodrigo <rodrigobdias@gmail.com>
2019-03-04 10:37:04 -0300

    Capítulo 10.3: Utilizando Templates de EMAIL e trabalhando com multiplos Profiles

--- src/main/java/br/com/alura/forum/service/infra/MailServiceException.java
+++ src/main/java/br/com/alura/forum/service/infra/MailServiceException.java
@@ -2,6 +2,8 @@ package br.com.alura.forum.service.infra;
 
 public class MailServiceException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public MailServiceException(String message, Throwable cause) {
 		super(message, cause);
 	}
