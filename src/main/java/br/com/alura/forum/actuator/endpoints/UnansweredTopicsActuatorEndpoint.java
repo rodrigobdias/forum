f5aa7db33c9b0f9782d1f44de3ef91f116e47ec4
Rodrigo <rodrigobdias@gmail.com>
2019-03-04 12:24:11 -0300

    Capítulo 13: Habilitando o Actuator e criando novo ENDPOINT

---
+++ src/main/java/br/com/alura/forum/actuator/endpoints/UnansweredTopicsActuatorEndpoint.java
@@ -0,0 +1,27 @@
+package br.com.alura.forum.actuator.endpoints;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import br.com.alura.forum.model.OpenTopicsByCategory;
+import br.com.alura.forum.repository.TopicRepository;
+
+@Endpoint(id="open-topics-by-category")
+@Component
+public class UnansweredTopicsActuatorEndpoint {
+
+	@Autowired
+	private TopicRepository topicRepository;
+
+	@ReadOperation
+	@Bean
+	public List<OpenTopicsByCategory> execute() {
+		return topicRepository.findOpenTopicsByCategory();
+	}
+
+}
