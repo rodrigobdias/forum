4b37bd57e26bdf0904521b1cd6acb67b36eec16d
rodrigobdias <rodrigobdias@gmail.com>
2020-06-04 16:11:52 -0300

    Começando com Spring Data JPA

--- src/main/java/br/com/alura/forum/model/Resposta.java
+++ src/main/java/br/com/alura/forum/model/Resposta.java
@@ -3,6 +3,13 @@ package br.com.alura.forum.model;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Resposta implements Serializable{
 
 	/**
@@ -10,10 +17,16 @@ public class Resposta implements Serializable{
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String mensagem;
+	
+	@ManyToOne
 	private Topico topico;
+	
 	private LocalDateTime dataCriacao = LocalDateTime.now();
+	
+	@ManyToOne
 	private Usuario autor;
 	private Boolean solucao = false;
 
