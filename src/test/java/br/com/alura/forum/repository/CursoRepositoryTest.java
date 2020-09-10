cd485639b33381bcabc23d909d740fc8a45b7ad1
rodrigobdias <rodrigobdias@gmail.com>
2020-09-10 16:14:33 -0300

    Testando o repository com outro banco de dados

--- src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,24 +5,36 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alura.forum.model.Curso;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
 	@Autowired
 	private CursoRepository repository;
 	
+	@Autowired
+	private TestEntityManager em;
+	
 	@Test
 	public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
 		
 		String nomeCurso = "HTML 5";
+		
+		// teste simulação
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("Programacao");
+		em.persist(html5);
+		
 		Curso curso = repository.findByNome(nomeCurso);
 		
 		Assert.assertNotNull(curso);
